1ebc7b8051198de0e53641262f1e597be0a6849d
TheProgramer85 <131801107+TheProgramer85@users.noreply.github.com>
2023-05-18 19:33:19 -0400

    Add files via upload

--- Tarea_2/src/Expendedor.java
+++ Tarea_2/src/Expendedor.java
@@ -1,4 +1,7 @@
-public class Expendedor{
+import java.awt.*;
+import javax.swing.*;
+
+public class Expendedor extends JPanel {
     private Deposito coca;
     private Deposito sprite;
     private Deposito super8;
@@ -11,87 +14,137 @@ public class Expendedor{
     public static final int SPRITE = 2;
     public static final int SUPER8 = 3;
     public static final int RAYITA = 4;
-    public Expendedor(int numProductos, int precioBebidas, int precioDulces) {
+    private int escala;
+    private int countserie;
+    private int countm100, countm500, countm1000, money;
+
+    public Expendedor(int numProductos, int precio, int x, int y, int escala) {
+        countserie = numProductos * 2;
+        countm100 = 0;
+        countm500 = 0;
+        countm1000 = 0;
+        money = 0;
         coca = new Deposito();
         sprite = new Deposito();
         super8 = new Deposito();
         rayita = new Deposito();
         monVu = new DepositoM();
 
-        this.precioB = precioBebidas;
-        this.precioD = precioDulces;
+        this.precioB = precio;
+        this.precioD = precio + 400;
 
-        for (int i = 0; i < numProductos; i++){
-            Producto cc = new CocaCola(100 + i);
+        for (int i = 0; i < numProductos; i++) {
+            CocaCola cc = new CocaCola(i, 0, 0, escala);
             coca.addProducto(cc);
-            Producto sp = new Sprite(200 + i);
+            Sprite sp = new Sprite(numProductos + i, 0, 0, escala);
             sprite.addProducto(sp);
-            Producto s8 = new Super8( 300 + i);
+            Super8 s8 = new Super8((numProductos * 2) + i, 0, 0, escala);
             super8.addProducto(s8);
-            Producto rt = new rayita( 400 + i);
+            Rayita rt = new Rayita((numProductos * 2) + i, 0, 0, escala);
             rayita.addProducto(rt);
         }
     }
-    public Producto comprarProducto(Moneda m, int n) throws ProductoNoDisponibleExcepcion, PagoIncorrectoExcepcion, PagoInsuficienteExcepcion {
+
+    public void refill(int n) {
+        if (n == 1) {
+            for (int i = countserie; i < countserie + 10; i++) {
+                CocaCola cc = new CocaCola(i, 0, 0, escala);
+                coca.addProducto(cc);
+            }
+            countserie = countserie + 10;
+        }
+        if (n == 2) {
+            for (int i = countserie; i < countserie + 10; i++) {
+                Sprite sp = new Sprite(countserie, 0, 0, escala);
+                sprite.addProducto(sp);
+            }
+            countserie = countserie + 10;
+        }
+        if (n == 3) {
+            for (int i = countserie; i < countserie + 10; i++) {
+                Super8 s8 = new Super8(countserie, 0, 0, escala);
+                super8.addProducto(s8);
+            }
+            countserie = countserie + 10;
+        }
+        if (n == 4) {
+            for (int i = countserie; i < countserie + 10; i++) {
+                Rayita rt = new Rayita(countserie, 0, 0, escala);
+                rayita.addProducto(rt);
+            }
+            countserie = countserie + 10;
+        }
+    }
+
+    public int getcountm100() {
+        return countm100;
+    }
+
+    public int getcountm500() {
+        return countm500;
+    }
+
+    public int getcountm1000() {
+        return countm1000;
+    }
+
+    public void setcountm100(int c) {
+        countm100 = c;
+    }
+
+    public void setcountm500(int c) {
+        countm500 = c;
+    }
+
+    public void setcountm1000(int c) {
+        countm1000 = c;
+    }
+
+    public Producto comprarProducto(Moneda m, int n) throws ProductoNoDisponibleExcepcion, PagoIncorrectoException, PagoInsuficienteException {
         this.codigo = n;
 
         if (m != null && m.getValor() >= precioB) {
-            switch(n){
-                case 1: Producto cc = coca.getProducto();
-                    if (cc != null) {
-                        throw new ProductoNoDisponibleExcepcion("Producto no disponible\n");
+            switch (n) {
+                case 1:
+                    Producto cc = coca.getProducto();
+                    if (cc != null) throw new ProductoNoDisponibleExcepcion("Producto no disponible\n");
 
-                        for (int i = 0; i < (m.getValor() - precioB) / 100; i++) {
-                            Moneda a = new Moneda100();
-                            monVu.addMoneda(a);
-                        }
-                        return cc;
-                    }
-                case 2: Producto sp = sprite.getProducto();
-                    if (sp != null) {
-                        throw new ProductoNoDisponibleExcepcion("Producto no disponible\n");
+                case 2:
+                    Producto sp = sprite.getProducto();
+                    if (sp != null) throw new ProductoNoDisponibleExcepcion("Producto no disponible\n");
 
-                        for (int i = 0; i < (m.getValor() - precioB) / 100; i++) {
-                            Moneda a = new Moneda100();
-                            monVu.addMoneda(a);
-                        }
-                        return sp;
-                    }
-                case 3: Producto s8 = super8.getProducto();
-                    if (s8 != null) {
-                        throw new ProductoNoDisponibleExcepcion("Producto no disponible\n");
+                case 3:
+                    Producto s8 = super8.getProducto();
+                    if (s8 != null) throw new ProductoNoDisponibleExcepcion("Producto no disponible\n");
 
-                        for (int i = 0; i < (m.getValor() - precioD) / 100; i++) {
-                            Moneda a = new Moneda100();
-                            monVu.addMoneda(a);
-                        }
-                        return s8;
-                    }
-                case 4: Producto rt = rayita.getProducto();
-                    if (rt != null) {
-                        throw new ProductoNoDisponibleExcepcion("Producto no disponible\n");
+                case 4:
+                    Producto rt = rayita.getProducto();
+                    if (rt != null) throw new ProductoNoDisponibleExcepcion("Producto no disponible\n");
 
-                        for (int i = 0; i < (m.getValor() - precioD) / 100; i++) {
-                            Moneda a = new Moneda100();
-                            monVu.addMoneda(a);
-                        }
-                        return rt;
-                    }
-                case 5: int money = m.getValor();
-                    if(m == null) throw new PagoIncorrectoExcepcion("Pago Invalido");
-                case 6: if(m.getValor() < precioB || m.getValor()< precioD) throw new PagoInsuficienteExcepcion("Cantidad de dinero incorrecta");
-                }
+                case 5:
+                    int money = m.getValor();
+                    if (m == null) throw new PagoIncorrectoException("Pago Incorrecto");
+
+                case 6:
+                    if (m.getValor() < precioB || m.getValor() < precioD)
+                        throw new PagoInsuficienteException("Cantidad de dinero incorrecta");
             }
-        else{
             monVu.addMoneda(m);
             return null;
         }
         monVu.addMoneda(m);
         return null;
     }
-    public int getPrecio(){
+
+    public int getPrecio() {
         if (codigo == 1 || codigo == 2) return precioB;
         else return precioD;
     }
-    public Moneda getVuelto(){ return monVu.getMoneda(); }
+
+    public Moneda getVuelto() {
+        return monVu.getMoneda();
+    }
+
+    public void IngresaMoneda(Object monedabyValor) {
+    }
 }
