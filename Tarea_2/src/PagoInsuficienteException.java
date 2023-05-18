92f823de6dfba696d0d64447b311abc5fbebf2ed
Maticruces <maticruces2021@inf.udec.cl>
2023-05-18 18:56:33 -0400

    agregacion de los diseños

---
+++ Tarea_2/src/PagoInsuficienteException.java
@@ -0,0 +1,7 @@
+
+/***Por modificar***/
+public class PagoInsuficienteException extends Exception{
+    public PagoInsuficienteException(String errorMesagge){
+        super(errorMesagge);
+    }
+}
