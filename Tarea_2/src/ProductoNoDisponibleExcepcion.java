7d35e369d02b5eee9aed2505cbc58d815a7a2509
TheProgramer85 <131801107+TheProgramer85@users.noreply.github.com>
2023-05-21 23:19:09 -0400

    ProductoNoDisponibleExcepcion.java

--- Tarea_2/src/ProductoNoDisponibleExcepcion.java
+++ Tarea_2/src/ProductoNoDisponibleExcepcion.java
@@ -1,3 +1,8 @@
+/*
+*@ProductoNoDisponibleExcepcion es una clase que interactua con Exception
+esta es utilizada para enviar un mensaje de error en el caso de que
+no quede disponible cierto producto en el expendedor.
+*/
 public class ProductoNoDisponibleExcepcion extends Exception{
     public ProductoNoDisponibleExcepcion(String errorMesagge){
         super(errorMesagge);
