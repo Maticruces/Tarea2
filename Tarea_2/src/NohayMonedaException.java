a2142d598a14d8a8be6bb846ede8f4afcf7564b5
TheProgramer85 <131801107+TheProgramer85@users.noreply.github.com>
2023-05-21 22:53:19 -0400

    NohayMonedaException.java

--- Tarea_2/src/NohayMonedaException.java
+++ Tarea_2/src/NohayMonedaException.java
@@ -1,3 +1,8 @@
+/*
+*@NoHayMonedaException es una clase que interactua con Exception,
+esta es utilizada para enviar un mensaje de error en el caso de que no
+se hubiera insertado ninguna moneda en el expendedor.
+*/
 public class NohayMonedaException extends Exception{
     public NohayMonedaException(String errorMesagge){
         super(errorMesagge);
