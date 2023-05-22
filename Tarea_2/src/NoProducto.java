14d59a1c8690ca4cc21f86ae6815d89dd83e8d3e
TheProgramer85 <131801107+TheProgramer85@users.noreply.github.com>
2023-05-21 22:49:06 -0400

    NoProducto.java

--- Tarea_2/src/NoProducto.java
+++ Tarea_2/src/NoProducto.java
@@ -1,5 +1,16 @@
 import java.awt.Graphics;
 import javax.swing.ImageIcon;
+
+/*
+*@NoProducto es una clase que representa el numero o codigo
+asociado a cierto producto del expendedor
+*@param: numSerie
+*@param: x
+*@param: y
+*@param: escala
+*@return: La posicion de cierto producto dentro del expendedor
+*/
+
 public class NoProducto extends Producto {
     public NoProducto(int numSerie, int x, int y, int escala){
         super(numSerie, x, y, escala);
