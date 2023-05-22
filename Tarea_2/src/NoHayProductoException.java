7150aa4fa5b9d1c160064eca245ee63aabd3f5b1
TheProgramer85 <131801107+TheProgramer85@users.noreply.github.com>
2023-05-21 22:45:27 -0400

    NoHayProductoException.java

--- Tarea_2/src/NoHayProductoException.java
+++ Tarea_2/src/NoHayProductoException.java
@@ -1,3 +1,8 @@
+/*
+*@NoHayProductoExcepcion es una clase que interactua con Exception
+esta es utilizada para enviar un mensaje de error en el caso de que
+no quede disponible cierto producto en el expendedor.
+*/
 public class NoHayProductoException extends Exception{
     public NoHayProductoException(String errorMesagge){
         super(errorMesagge);
